ce61c3c5a715983bc75ce3323d26e128d4603464
Sanjay <sanjayrjs16@gmail.com>
2025-05-07 22:00:14 +0530

    Site map added + few tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sanjayrajesh.in',
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
